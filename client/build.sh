f76fcb5b5d4a8a3beaeb537b433302bd7b558bd0
Addhyan Kohli <addhyan@gmail.com>
2025-05-30 22:03:58 -0700

    client perm change

--- client/build.sh
+++ client/build.sh
@@ -3,8 +3,17 @@
 # Install dependencies
 npm install
 
+# Ensure react-scripts is installed globally (as a backup)
+npm install -g react-scripts
+
+# Make node_modules/.bin executable
+chmod +x node_modules/.bin/*
+
+# Make react-scripts executable specifically
+chmod +x node_modules/.bin/react-scripts
+
 # Clear any previous build
 rm -rf build
 
 # Set environment variable and run build
-CI=false npm run build 
+CI=false ./node_modules/.bin/react-scripts build 
