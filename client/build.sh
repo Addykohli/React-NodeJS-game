b800d82a3255be616844b48d9820d74404200aea
Addhyan Kohli <addhyan@gmail.com>
2025-05-30 22:28:57 -0700

    deployment debugging

--- client/build.sh
+++ client/build.sh
@@ -3,5 +3,5 @@
 # Install dependencies
 npm install
 
-# Build the React app using npx
-CI=false npx react-scripts build 
+# Build the React app using direct path
+CI=false node ./node_modules/react-scripts/scripts/build.js 
