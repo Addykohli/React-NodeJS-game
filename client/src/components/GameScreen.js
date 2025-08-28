c36ed8db024c9bdcc21612af3aec294d2ab14462
Addhyan Kohli <addhyan@gmail.com>
2025-08-27 19:20:47 -0700

    master terminal 1

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import bgImage from '../assets/bg.png';
 import startIcon from '../assets/start.png';
 import hotelIcon from '../assets/hotel.png';
